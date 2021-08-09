eb842196db9b1dc0dbd4d7e77dffa71393f87a4d
Collen Maphike <Mavetane>
2021-08-09 18:31:18 +0200

    added google fonts

--- client/components/Meta.js
+++ client/components/Meta.js
@@ -12,6 +12,11 @@ const Meta = ({ title, keywords, description }) => {
             <link rel='icon' href='/favicon.ico' />
 
             <title>{title}</title>
+            <link rel="preconnect" href="https://fonts.googleapis.com" />
+            <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+            <link href="https://fonts.googleapis.com/css2?family=Abril+Fatface&family=Roboto+Condensed:wght@300&display=swap" rel="stylesheet"/>
+            <link href="https://fonts.googleapis.com/css2?family=Noto+Serif&display=swap" rel="stylesheet" />
+
         </Head>
     )
 }
